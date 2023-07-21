2233f85314cf60caa8816b17a7f810e47e4f6818
Quang <quang02042002@gmail.com>
2023-07-21 21:32:41 +0700

    123

---
+++ app/src/main/java/com/example/mygrocerystore/activities/RegistrationActivity.java
@@ -0,0 +1,106 @@
+package com.example.mygrocerystore.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.models.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    Button signUp;
+    EditText name, email,password;
+    TextView signIn;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        auth=FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        signUp=findViewById(R.id.reg_btn);
+        name=findViewById(R.id.name_reg);
+        email=findViewById(R.id.email_reg);
+        password=findViewById(R.id.password_reg);
+        signIn=findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createUser();
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void createUser() {
+        String userName=name.getText().toString();
+        String userEmail=email.getText().toString();
+        String userPassword=password.getText().toString();
+
+        if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this,"Name is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this,"Email is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this,"Password is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(userPassword.length()<6){
+            Toast.makeText(this,"Password length must >6",Toast.LENGTH_SHORT).show();
+        }
+        //create user
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            UserModel userModel = new UserModel(userName,userEmail,userPassword);
+                            String id =  task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(userModel);
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(RegistrationActivity.this,"Registration success",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            progressBar.setVisibility(View.VISIBLE);
+                            Toast.makeText(RegistrationActivity.this,"Registration failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+}
---
+++ app/src/main/java/com/example/mygrocerystore/ui/home/HomeFragment.java
@@ -0,0 +1,227 @@
+package com.example.mygrocerystore.ui.home;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mygrocerystore.R;
+import com.example.mygrocerystore.adapters.HomeAdapter;
+import com.example.mygrocerystore.adapters.PopularAdapters;
+import com.example.mygrocerystore.adapters.RecommendedAdapter;
+import com.example.mygrocerystore.adapters.ViewAllAdapter;
+import com.example.mygrocerystore.databinding.FragmentHomeBinding;
+import com.example.mygrocerystore.models.HomeCategory;
+import com.example.mygrocerystore.models.PopularModel;
+import com.example.mygrocerystore.models.RecommendedModel;
+import com.example.mygrocerystore.models.ViewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+
+    ScrollView scrollView;
+    ProgressBar progressBar;
+
+    RecyclerView popularRec,homeCatRec,recommendedRec;
+    FirebaseFirestore db;
+    //popular
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
+
+    /////search
+    EditText search_box;
+    private List<ViewAllModel> viewAllModelList;
+    private RecyclerView recyclerViewSearch;
+    private ViewAllAdapter viewAllAdapter;
+
+
+    //category
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
+    //recommended
+
+    RecommendedAdapter recommendedAdapter;
+    List<RecommendedModel> recommendedModelList;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root=inflater.inflate(R.layout.fragment_home,container,false);
+        db=FirebaseFirestore.getInstance();
+
+
+        popularRec = root.findViewById(R.id.pop_rec);
+        homeCatRec = root.findViewById(R.id.explore_rec);
+        recommendedRec = root.findViewById(R.id.recommended_rec);
+        scrollView = root.findViewById(R.id.scroll_view);
+        progressBar = root.findViewById(R.id.progressbar);
+        
+        progressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+
+
+        //Popular Items
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        popularModelList = new ArrayList<>();
+        popularAdapters=new PopularAdapters(getActivity(),popularModelList);
+        popularRec.setAdapter(popularAdapters);
+
+        db.collection("PopularProducts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel = document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapters.notifyDataSetChanged();
+
+                                progressBar.setVisibility(View.GONE);
+                                scrollView.setVisibility(View.VISIBLE);
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+        //Category
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryList = new ArrayList<>();
+        homeAdapter=new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        db.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                HomeCategory homeCategory = document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    //Recommended
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        recommendedModelList = new ArrayList<>();
+        recommendedAdapter=new RecommendedAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        db.collection("Recommended")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                RecommendedModel recommendedModel = document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Error"+task.getException(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
+        ///////search view
+        recyclerViewSearch = root.findViewById(R.id.search_rec);
+        search_box =root.findViewById(R.id.search_box);
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(getContext(),viewAllModelList);
+        recyclerViewSearch.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewSearch.setAdapter(viewAllAdapter);
+        recyclerViewSearch.setHasFixedSize(true);
+        search_box.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if(s.toString().isEmpty()){
+                    viewAllModelList.clear();
+                    viewAllAdapter.notifyDataSetChanged();
+                } else {
+                    searchProduct(s.toString());
+                }
+            }
+        });
+
+
+
+        return root;
+    }
+
+    private void searchProduct(String type) {
+         if(!type.isEmpty()){
+
+             db.collection("AllProducts").whereEqualTo("type",type).get()
+                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                         @Override
+                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                             if(task.isSuccessful() && task.getResult() != null){
+                                 viewAllModelList.clear();
+                                 viewAllAdapter.notifyDataSetChanged();
+                                 for(DocumentSnapshot doc : task.getResult().getDocuments()){
+                                     ViewAllModel viewAllModel = doc.toObject(ViewAllModel.class);
+                                     viewAllModelList.add(viewAllModel);
+                                     viewAllAdapter.notifyDataSetChanged();
+                                 }
+
+                             }
+
+
+
+
+                         }
+                     });
+
+         }
+    }
+
+}
